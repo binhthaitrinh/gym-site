fc8f9b7e37a268527024578e6881e3bbe04f4f11
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-04-12 22:50:01 +0700

    fix button for Ipad, fix animation for ipad

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import 'waypoints/lib/jquery.waypoints.min.js';
+import WOW from 'wow.js';
 import SmoothScroll from 'smooth-scroll';
 
 // const feature = new Waypoint({
@@ -16,16 +17,35 @@ import SmoothScroll from 'smooth-scroll';
 //     },
 //     offset: '50%'
 // });
-$('.js--wp-1').waypoint(function(direction) {
-    $('.js--wp-1').addClass('animated fadeIn');
-}, {
-    offset: '50%'
-});
+// $('.js--wp-1').waypoint(function(direction) {
+//     $('.js--wp-1').addClass('animated fadeIn');
+// }, {
+//     offset: '50%'
+// });
 
-$('.js--wp-2').waypoint(function(direction) {
-    $('.js--wp-2').addClass('animated fadeInUp');
-}, {
-    offset: '50%'
-});
+// $('.js--wp-2').waypoint(function(direction) {
+//     $('.js--wp-2').addClass('animated fadeInUp');
+// }, {
+//     offset: '50%'
+// });
 
 var scroll = new SmoothScroll('a[href*="#"]');
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  const abc = new WOW().init();
+
+  
