7e7c678cafc92853164caf3ecaf08b1d488c6caa
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-04-12 21:24:58 +0700

    Add jQuery

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,31 @@
-import 'waypoints/lib/noframework.waypoints.js';
+import 'waypoints/lib/jquery.waypoints.min.js';
 import SmoothScroll from 'smooth-scroll';
 
-const feature = new Waypoint({
-    element: document.querySelector('.js--wp-1'),
-    handler: () => {
-        document.querySelector('.js--wp-1').classList.add('animated', 'fadeIn');
-    },
+// const feature = new Waypoint({
+//     element: document.querySelector('.js--wp-1'),
+//     handler: () => {
+//         document.querySelector('.js--wp-1').classList.add('animated', 'fadeIn');
+//     },
+//     offset: '50%'
+// });
+
+// const contactUs = new Waypoint({
+//     element: document.querySelector('.js--wp-2'),
+//     handler: () => {
+//         document.querySelector('.js--wp-2').classList.add('animated', 'fadeIn');
+//     },
+//     offset: '50%'
+// });
+$('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+}, {
     offset: '50%'
 });
 
-const contactUs = new Waypoint({
-    element: document.querySelector('.js--wp-2'),
-    handler: () => {
-        document.querySelector('.js--wp-2').classList.add('animated', 'fadeIn');
-    },
+$('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+}, {
     offset: '50%'
 });
-
 
 var scroll = new SmoothScroll('a[href*="#"]');
